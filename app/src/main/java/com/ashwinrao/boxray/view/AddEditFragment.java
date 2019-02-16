b0f18730d9d31bbfe23b381fca2b2e85dd35eb03
Ashwin Rao <ashwinravrao@gmail.com>
2019-02-15 22:57:52 -0600

    More view encapsulation

--- app/src/main/java/com/ashwinrao/boxray/view/AddEditFragment.java
+++ app/src/main/java/com/ashwinrao/boxray/view/AddEditFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -14,11 +15,14 @@ import android.widget.TextView;
 import com.ashwinrao.boxray.R;
 import com.ashwinrao.boxray.databinding.FragmentAddEditBinding;
 import com.ashwinrao.boxray.util.Utilities;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
@@ -29,7 +33,10 @@ public class AddEditFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ((MainActivity) Objects.requireNonNull(getActivity())).getFloatingActionButton().setVisibility(View.GONE);
+
+        FloatingActionButton fab = ((MainActivity) getActivity()).getFloatingActionButton();
+        if(fab != null) { fab.setVisibility(View.GONE); }
+
         mOriginalSoftInputMode = Utilities.applyFragmentSoftInput(getActivity(), null);
     }
 
@@ -38,16 +45,46 @@ public class AddEditFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final FragmentAddEditBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_edit, container, false);
         configureInputFields(binding);
+        configureAddItemField(binding);
+
+        binding.choosePhotoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO add image picker with option for launching camera
+                Snackbar.make(Objects.requireNonNull(getActivity()).getWindow().getDecorView().findViewById(R.id.drawer_layout), "Add more items", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
         return binding.getRoot();
     }
 
-    private void configureInputFields(@NonNull FragmentAddEditBinding binding) {
+    private void configureAddItemField(@NonNull final FragmentAddEditBinding binding) {
+        binding.addItemFieldEditable.setHint(R.string.add_item_field_title);
+        binding.addItemFieldEditable.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.toString().length() > 0) {
+                    binding.addItemImageView.setImageResource(R.drawable.ic_add_item_enabled);
+                } else {
+                    binding.addItemImageView.setImageResource(R.drawable.ic_add_item_disabled);
+                }
+            }
+        });
+    }
+
+    private void configureInputFields(@NonNull final FragmentAddEditBinding binding) {
         String[] field_titles = {getString(R.string.name_field_title), getString(R.string.source_field_title), getString(R.string.dest_field_title)};
-        View[] views = {binding.nameInput, binding.sourceInput, binding.destInput};
+        final View[] views = {binding.nameInput, binding.sourceInput, binding.destInput};
 
         for (int i = 0; i < views.length; i++) {
             final TextView hint = views[i].findViewById(R.id.input_field_hint);
-            EditText editable = views[i].findViewById(R.id.input_field_editable);
+            final EditText editable = views[i].findViewById(R.id.input_field_editable);
 
             hint.setText(field_titles[i]);
             editable.setHint(field_titles[i]);
