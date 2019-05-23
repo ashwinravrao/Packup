fc31be2bcfaff60bc6dddb0e3d0c579c4ac533b0
Ashwin Rao <ashwinravrao@gmail.com>
2019-05-23 15:54:17 -0500

    Implement search functionality on boxes

--- app/src/main/java/com/ashwinrao/boxray/view/ListFragment.java
+++ app/src/main/java/com/ashwinrao/boxray/view/ListFragment.java
@@ -3,6 +3,8 @@ package com.ashwinrao.boxray.view;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,6 +13,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Toast;
 
 import com.ashwinrao.boxray.Boxray;
 import com.ashwinrao.boxray.R;
@@ -18,6 +22,7 @@ import com.ashwinrao.boxray.data.Box;
 import com.ashwinrao.boxray.databinding.FragmentListBinding;
 import com.ashwinrao.boxray.util.BackNavigationListener;
 import com.ashwinrao.boxray.util.DestinationDialog;
+import com.ashwinrao.boxray.util.Utilities;
 import com.ashwinrao.boxray.view.adapter.ListAdapter;
 import com.ashwinrao.boxray.viewmodel.BoxViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -32,7 +37,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -40,7 +47,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import javax.inject.Inject;
@@ -51,7 +58,6 @@ public class ListFragment extends Fragment implements NavigationView.OnNavigatio
     private ListAdapter listAdapter;
     private List<Box> localBoxes;
     private LiveData<List<Box>> boxesLD;
-    private FragmentListBinding binding;
     private RecyclerView recyclerView;
     private DrawerLayout drawer;
 
@@ -76,7 +82,7 @@ public class ListFragment extends Fragment implements NavigationView.OnNavigatio
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false);
+        final FragmentListBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false);
 
         AppCompatActivity parent = ((MainActivity) getActivity());
         Toolbar toolbar = binding.includeAppBar.toolbar;
@@ -85,6 +91,7 @@ public class ListFragment extends Fragment implements NavigationView.OnNavigatio
 
         drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
+        navigationView.setCheckedItem(R.id.home);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 getActivity(), drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -98,9 +105,9 @@ public class ListFragment extends Fragment implements NavigationView.OnNavigatio
         });
 
         recyclerView = binding.includeAppBar.recyclerView;
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        addItemDecoration(recyclerView);
         listAdapter = new ListAdapter(Objects.requireNonNull(getActivity()));
-//        listAdapter.registerToolBarListener(this);
         recyclerView.setAdapter(listAdapter);
 
         boxesLD.observe(this, boxes -> {
@@ -116,27 +123,75 @@ public class ListFragment extends Fragment implements NavigationView.OnNavigatio
         return binding.getRoot();
     }
 
+    private void addItemDecoration(RecyclerView recyclerView) {
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                int position = parent.getChildAdapterPosition(view);
+                int spanCount = 2;
+                int spacing = (int) Utilities.dpToPx(Objects.requireNonNull(getActivity()), 16f);
+
+                if (position >= 0) {
+                    int column = position % spanCount;
+
+                    outRect.left = spacing - column * spacing / spanCount;
+                    outRect.right = (column + 1) * spacing / spanCount;
+
+                    if (position < spanCount) {
+                        outRect.top = spacing;
+                    }
+                    outRect.bottom = spacing;
+                } else {
+                    outRect.left = 0;
+                    outRect.right = 0;
+                    outRect.top = 0;
+                    outRect.bottom = 0;
+                }
+            }
+        });
+    }
+
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_toolbar_list, menu);
+
+        MenuItem search = menu.findItem(R.id.toolbar_search);
+        SearchView searchView = (SearchView) search.getActionView();
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        searchView.setQueryHint("Search boxes");
+        searchView.setPadding((int) Utilities.dpToPx(Objects.requireNonNull(getActivity()), -16f), 0, 0, 0);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                listAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.toolbar_search) {
-            return true;
+
+        switch(item.getItemId()) {
+            case R.id.toolbar_search_by_image:
+                Toast.makeText(getActivity(), "Todo: implement searching by image (CV)", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.toolbar_sort:
+                Toast.makeText(getActivity(), "Todo: implement sorting", Toast.LENGTH_SHORT).show();
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.home) {
-            // Handle the camera action
-        } else if (item.getItemId() == R.id.settings) {
-
-        }
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
@@ -156,14 +211,6 @@ public class ListFragment extends Fragment implements NavigationView.OnNavigatio
         dialog.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.colorAccent, Objects.requireNonNull(getActivity()).getTheme()));
     }
 
-    private void resetMultiSelectMode() {
-//        restoreDefaultAppBar();
-        listAdapter.disableMultiSelectMode();
-        listAdapter.setBoxes(localBoxes);
-        recyclerView.setAdapter(listAdapter);
-    }
-
-
     private DialogInterface.OnClickListener dialogPositive() {
         return (dialog, which) -> {
             Snackbar.make(Objects.requireNonNull(getView()), "Saving", Snackbar.LENGTH_LONG).show();    // todo: replace with save address for geofencing
@@ -174,18 +221,11 @@ public class ListFragment extends Fragment implements NavigationView.OnNavigatio
         return (dialog, which) -> dialog.cancel();
     }
 
-//    @Override
-//    public void restoreDefaultAppBar() {
-//        binding.contextualAppBar.setVisibility(View.GONE);
-//    }
-
-
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         }
-//        resetMultiSelectMode();
     }
 
     @Override
--- app/src/main/java/com/ashwinrao/boxray/view/adapter/ListAdapter.java
+++ app/src/main/java/com/ashwinrao/boxray/view/adapter/ListAdapter.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 
 import com.ashwinrao.boxray.R;
 import com.ashwinrao.boxray.data.Box;
 import com.ashwinrao.boxray.databinding.ViewholderBoxBinding;
+import com.ashwinrao.boxray.databinding.ViewholderBoxNewBinding;
 import com.ashwinrao.boxray.util.ContextualAppBarListener;
 import com.ashwinrao.boxray.view.DetailFragment;
 import com.ashwinrao.boxray.view.MainActivity;
@@ -20,100 +23,99 @@ import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ListAdapter extends RecyclerView.Adapter<ListAdapter.BoxViewHolder> {
+import javax.inject.Singleton;
 
-        private Context context;
-        private List<Box> boxes;
-        private List<Box> selected = new ArrayList<>();
-        private boolean multiSelectMode = false;
-        private ContextualAppBarListener listener;
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.BoxViewHolder> implements Filterable {
 
-        public ListAdapter(@NonNull Context context) {
-            this.context = context;
-        }
+    private Context context;
+    private List<Box> boxes;
+    private List<Box> boxesCopy;
 
-        public void setBoxes(List<Box> boxes) {
-            this.boxes = boxes;
-        }
+    public ListAdapter(@NonNull Context context) {
+        this.context = context;
+    }
 
-        public void registerToolBarListener(ContextualAppBarListener listener) {
-            this.listener = listener;
-        }
+    public void setBoxes(List<Box> boxes) {
+        this.boxes = boxes;
+        this.boxesCopy = new ArrayList<>(boxes);
+    }
 
-        public void disableMultiSelectMode() {
-            selected.clear();
-            multiSelectMode = !multiSelectMode;
-        }
+    @NonNull
+    @Override
+    public BoxViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ViewholderBoxNewBinding binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.viewholder_box_new, parent, false);
+        return new BoxViewHolder(binding);
+    }
 
-        @NonNull
+    @Override
+    public void onBindViewHolder(@NonNull BoxViewHolder holder, int position) {
+        Box box = boxes.get(position);
+        holder.binding.setBox(box);
+    }
+
+    @Override
+    public int getItemCount() {
+        return boxes == null ? 0 : boxes.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+
+    private Filter filter = new Filter() {
+
         @Override
-        public BoxViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            ViewholderBoxBinding binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.viewholder_box, parent, false);
-            return new BoxViewHolder(binding);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull BoxViewHolder holder, int position) {
-            Box box = boxes.get(position);
-            holder.binding.setBox(box);
-            holder.binding.boxNumberTextView.setText(String.valueOf(box.getId()));
-            holder.binding.numItemsTextView.setText(box.getContents() == null ? "No items" : context.getString(R.string.box_num_items_placeholder, box.getContents().size()));
-        }
-
-        @Override
-        public int getItemCount() {
-            return boxes == null ? 0 : boxes.size();
-        }
-
-        private void toggleSelection(@NonNull ViewGroup root, @NonNull Box box) {
-            if(selected.contains(box)) {
-                root.setBackground(context.getDrawable(R.drawable.background_box_viewholder));
-                selected.remove(box);
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<Box> filtered = new ArrayList<>();
+            if(constraint == null || constraint.length() < 1) {
+                filtered.addAll(boxesCopy);
             } else {
-                root.setBackground(context.getDrawable(R.drawable.background_box_viewholder_selected));
-                selected.add(box);
-            }
-        }
-
-        public class BoxViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
-
-            private ViewholderBoxBinding binding;
-
-            public BoxViewHolder(@NonNull ViewholderBoxBinding binding) {
-                super(binding.getRoot());
-                this.binding = binding;
-                this.binding.getRoot().setOnClickListener(this);
-                this.binding.getRoot().setOnLongClickListener(this);
-            }
-
-            @Override
-            public void onClick(View v) {
-                if(!multiSelectMode) {
-                    Bundle bundle = new Bundle();
-                    bundle.putInt("ID", boxes.get(getAdapterPosition()).getId());
-                    DetailFragment detail = new DetailFragment();
-                    detail.setArguments(bundle);
-
-                    ((MainActivity) context)
-                            .getSupportFragmentManager()
-                            .beginTransaction()
-                            .addToBackStack(null)
-                            .setCustomAnimations(R.anim.slide_in_from_right, R.anim.stay_still, R.anim.stay_still, R.anim.slide_out_to_right)
-                            .replace(R.id.fragment_container, detail)
-                            .commit();
-                } else {
-                    toggleSelection(binding.rootViewGroup, boxes.get(getAdapterPosition()));
+                String filterText = constraint.toString().toLowerCase().trim();
+                for(Box box : boxesCopy) {
+                    if(box.getName().contains(filterText)) {
+                        filtered.add(box);
+                    }
                 }
             }
-
-            @Override
-            public boolean onLongClick(View v) {
-                if(!multiSelectMode) {
-                    listener.overlayContextualAppBar();
-                    toggleSelection(binding.rootViewGroup, boxes.get(getAdapterPosition()));
-                    multiSelectMode = !multiSelectMode;
-                }
-                return true;
-            }
+            FilterResults results = new FilterResults();
+            results.values = filtered;
+            return results;
         }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            boxes.clear();
+            boxes.addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+    };
+
+    public class BoxViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private ViewholderBoxNewBinding binding;
+
+        public BoxViewHolder(@NonNull ViewholderBoxNewBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+            this.binding.getRoot().setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Bundle bundle = new Bundle();
+            bundle.putInt("ID", boxes.get(getAdapterPosition()).getId());
+            DetailFragment detail = new DetailFragment();
+            detail.setArguments(bundle);
+
+            ((MainActivity) context)
+                    .getSupportFragmentManager()
+                    .beginTransaction()
+                    .addToBackStack(null)
+                    .setCustomAnimations(R.anim.slide_in_from_right, R.anim.stay_still, R.anim.stay_still, R.anim.slide_out_to_right)
+                    .replace(R.id.fragment_container, detail)
+                    .commit();
+        }
+    }
 }
