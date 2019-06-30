6e2c106358ae70dcf8a12c3cc40b5a1eabb2635b
Ashwin Rao <ashwinravrao@gmail.com>
2019-06-29 22:42:28 -0500

    Implement on-device text recognition via Firebase ML Kit

--- app/src/main/java/com/ashwinrao/locrate/view/fragment/CameraFragment.java
+++ app/src/main/java/com/ashwinrao/locrate/view/fragment/CameraFragment.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.VibrationEffect;
@@ -37,26 +38,17 @@ import com.ashwinrao.locrate.R;
 import com.ashwinrao.locrate.databinding.FragmentCameraBinding;
 import com.ashwinrao.locrate.util.callback.BackNavCallback;
 import com.ashwinrao.locrate.view.activity.CameraActivity;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.services.vision.v1.Vision;
-import com.google.api.services.vision.v1.VisionRequestInitializer;
-import com.google.api.services.vision.v1.model.AnnotateImageRequest;
-import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
-import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
-import com.google.api.services.vision.v1.model.Feature;
-import com.google.api.services.vision.v1.model.Image;
-import com.google.api.services.vision.v1.model.TextAnnotation;
-
-import org.apache.commons.io.IOUtils;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import static android.content.Context.VIBRATOR_SERVICE;
@@ -179,57 +171,36 @@ public class CameraFragment extends Fragment implements Toolbar.OnMenuItemClickL
                     }
 
                     if(useCase == 1) {
-
-                        AsyncTask.execute(() -> {
-                            try {
-                                Vision.Builder visionBuilder = new Vision.Builder(
-                                        new NetHttpTransport(),
-                                        new AndroidJsonFactory(),
-                                        null)
-                                        .setApplicationName("Locrate");
-                                visionBuilder.setVisionRequestInitializer(new VisionRequestInitializer(CLOUD_VISION_KEY));
-
-                                Vision vision = visionBuilder.build();
-
-                                InputStream inputStream = new FileInputStream(file);
-                                byte[] photoData = IOUtils.toByteArray(inputStream);
-                                inputStream.close();
-
-                                Image inputImage = new Image();
-                                inputImage.encodeContent(photoData);
-
-                                Feature desiredFeature = new Feature();
-                                desiredFeature.setType("TEXT_DETECTION");
-
-                                AnnotateImageRequest request = new AnnotateImageRequest();
-                                request.setImage(inputImage);
-                                request.setFeatures(Arrays.asList(desiredFeature));
-
-                                BatchAnnotateImagesRequest batchRequest = new BatchAnnotateImagesRequest();
-                                batchRequest.setRequests(Arrays.asList(request));
-
-                                BatchAnnotateImagesResponse batchResponse = vision.images().annotate(batchRequest).execute();
-
-                                TextAnnotation textAnnotation = batchResponse.getResponses().get(0).getFullTextAnnotation();
-                                Log.d(TAG, String.format("analyze: textAnnotation is null? %b", textAnnotation == null));
-                                Log.d(TAG, "onImageSaved: text annotation is: " + textAnnotation.getText());
-                                getActivity().runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Toast.makeText(getContext(), textAnnotation.getText(), Toast.LENGTH_SHORT).show();
+                        try {
+                            final FirebaseVisionImage image = FirebaseVisionImage.fromFilePath(getContext(), Uri.fromFile(file));
+                            final FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+                            recognizer.processImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                                @Override
+                                public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                                    final List<FirebaseVisionText.TextBlock> blocks = firebaseVisionText.getTextBlocks();
+                                    if(blocks.size() == 0) {
+                                        Toast.makeText(getContext(), "No text found", Toast.LENGTH_SHORT).show();
+                                        return;
                                     }
-                                });
-
-                            } catch (FileNotFoundException e) {
-                                Log.e(TAG, "run: file at path " + file.getAbsolutePath() + " not found");
-                            } catch (IOException ie) {
-                                Log.e(TAG, "run: IOException" + ie.getMessage());
-                            }
-                        });
-
-//                        final TextAnnotation annotation = new ImageAnalyzer(file.getAbsolutePath()).analyze();
-//                        Toast.makeText(getContext(), annotation.getText(), Toast.LENGTH_LONG).show();
-//                        finishUpActivity();
+                                    for(int i = 0; i < blocks.size(); i++) {
+                                        final List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+                                        for(int j = 0; j < lines.size(); j++) {
+                                            final List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+                                            for(int k = 0; k < elements.size(); k++) {
+                                                Toast.makeText(getContext(), elements.get(k).getText(), Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    }
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    e.printStackTrace();
+                                }
+                            });
+                        } catch (IOException e) {
+                            Log.e(TAG, "onImageSaved: " + e.getMessage());
+                        }
                     } else {
                         // Notify user of saved image
                         Toast toast = Toast.makeText(getContext(), "Saved", Toast.LENGTH_SHORT);
